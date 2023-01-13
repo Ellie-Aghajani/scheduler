d8927e69a97ae0024211a5b34ad2fa09d8b05139
Ellie-Aghajani <z.aqajani@gmail.com>
2023-01-12 22:07:14 -0800

    header updated

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,4 +1,15 @@
-<header className="appointment__time">
-  <h4 className="text--semi-bold">12pm</h4>
-  <hr className="appointment__separator" />
-</header>
+import React from "react";
+
+
+
+const Header = function (props) {
+
+  return (
+    <header className="appointment__time">
+      <h4 className="text--semi-bold">{props.time}</h4>
+      <hr className="appointment__separator" />
+    </header>
+  );
+};
+
+export default Header;
