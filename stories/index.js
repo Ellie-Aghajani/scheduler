582eb218fa51a85e61595881ec0d2675d96ed879
Ellie-Aghajani <z.aqajani@gmail.com>
2023-01-12 22:00:29 -0800

    appointment story added

--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Appointment from "components/Appointment/index.js";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
@@ -43,3 +43,9 @@ storiesOf("Button", module)
       onChange={action("setInterviewer")}
     />
   ));
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
