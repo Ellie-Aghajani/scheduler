d522a0dabbf73701a604bfe6af17c5f43637082f
Ellie-Aghajani <z.aqajani@gmail.com>
2023-01-17 16:09:32 -0800

    used map to retrieve a new array of interviewers

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,28 +2,30 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
-const formatSpots = function (spots) {
-  if (spots === 1) {
-    return spots + ` spot`;
-  }
-  return `${spots === 0 ? "no" : spots} spots`;
-};
-
 export default function DayListItem(props) {
-  const { name, spots, selected, setDay } = props;
   const dayClass = classNames("day-list__item", {
-    "day-list__item--selected": selected,
-    "day-list__item--full": spots === 0,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
   });
-
-  const clickHandler = () => setDay(name);
-
-  return (
-    <li className={dayClass} onClick={clickHandler} data-testid="day">
-      <h2 className="text--regular">{name}</h2>
-      <h3 selected={selected} className="text--light">
-        {formatSpots(spots)} remaining
-      </h3>
-    </li>
-  );
-}
+  
+  const formatSpots = function(props) {
+    
+    if (props.spots === 0) {
+      return 'no spots remaining';
+    } 
+    if (props.spots === 1) {
+      return '1 spot remaining';
+    } 
+    if (props.spots > 1) {
+      return `${props.spots} spots remaining`;
+    }
+    
+  
+  }
+    return (
+      <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+        <h2 className="text--regular">{props.name}</h2> 
+        <h3 className="text--light">{formatSpots(props)}</h3>
+      </li>
+    );
+  }
