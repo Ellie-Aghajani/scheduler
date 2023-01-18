451ecdb63fb6a1911daf17d04071af50d8a6e13e
Ellie-Aghajani <z.aqajani@gmail.com>
2023-01-17 16:38:23 -0800

    shortened code to increase readability

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,40 +1,27 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode, replace = false) {
+  const transition = (newMode, replace) => {
+    if (replace === true) {
+      setMode(newMode);
+    } else {
+    setHistory([...history, newMode])
     setMode(newMode);
-    if (replace) {
-      setHistory((prev) => {
-        const newHistory = [...prev];
-
-        newHistory.splice(newHistory.length - 1, 1, newMode);
-        return newHistory;
-      });
-      return;
-    };
-
-    setHistory((prev) => {
-      const newHistory = [...prev];
-      newHistory.push(newMode);
-      return newHistory;
-    });
+    }
   };
 
-  function back() {
-    const newHistory = [...history];
-    if (newHistory.length > 1) {
-      newHistory.pop();
-      setMode(history[newHistory.length - 1]);
-      setHistory(newHistory);
-    };
+  const back = () => {
+    if (history.length > 1) {
+      const copyHistory = [...history];
+      copyHistory.pop();
+      setHistory(copyHistory);
+      setMode(copyHistory[copyHistory.length - 1]);
+    }
+
   };
 
-  return {
-    mode,
-    transition,
-    back,
-  };
+  return { mode, transition, back };
 };
